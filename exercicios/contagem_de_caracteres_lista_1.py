e7f04b6174f0fea8fbae45b1e2e5aef3374aed75
faleite <fabricio_2310@hotmail.com>
2021-03-19 21:37:27 +0000

    Istaladas Libs Pytest, Flake8, e Requests

--- exercicios/contagem_de_caracteres_lista_1.py
+++ exercicios/contagem_de_caracteres_lista_1.py
@@ -1,4 +1,4 @@
-def f_contar_c(s): #criar função que conta casacteres da string 's'(s)
+def f_contar_c(s):  # criar função que conta casacteres da string 's'(s)
     """Função contar caracteres de uma string
     ##Quais são os caracteres de uma string, e quantas vezes cada caracter aparece na mesma?
     Ex:
@@ -21,20 +21,22 @@ def f_contar_c(s): #criar função que conta casacteres da string 's'(s)
     :param s: string a ser contada
 
     """
-    f_c_ordenados = sorted(s) #f = função, c = caracteres, s = string
-    f_def_c_ant = f_c_ordenados[0] #[0] = repr. 1º c #manter ref p c anterior da s enqto o c anterior for = ao c corrente, somar num de vez q esse c aparece
+    f_c_ordenados = sorted(s)  # f = função, c = caracteres, s = string
+    f_def_c_ant = f_c_ordenados[
+        0]  # [0] = repr. 1º c #manter ref p c anterior da s enqto o c anterior for = ao c corrente, somar num de vez q esse c aparece
     valor_c = 1
-    for c in f_c_ordenados[1:]: #iteração dos c subsequntes, q é a partir de [1] (no caso o 1º c é o [0], o 2º c é [1])
+    for c in f_c_ordenados[
+             1:]:  # iteração dos c subsequntes, q é a partir de [1] (no caso o 1º c é o [0], o 2º c é [1])
         if c == f_def_c_ant:
             valor_c += 1
-        else: #então se não houver nenhuma das situaçõaes acima:
-            print(f'{f_def_c_ant}: {valor_c}') #eu vou imprimir o c ant e o seu valor
-            f_def_c_ant = c #o c anterior passa a ref o c atual
-            valor_c = 1 #valor do novo c da lista
-    print(f'{f_def_c_ant}: {valor_c}') #ref ao ultimo c
+        else:  # então se não houver nenhuma das situaçõaes acima:
+            print(f'{f_def_c_ant}: {valor_c}')  # eu vou imprimir o c ant e o seu valor
+            f_def_c_ant = c  # o c anterior passa a ref o c atual
+            valor_c = 1  # valor do novo c da lista
+    print(f'{f_def_c_ant}: {valor_c}')  # ref ao ultimo c
 
 
-if __name__ == '__main__': #
+if __name__ == '__main__':  #
     f_contar_c('fabricio')
-    print() #serve para pular linha
+    print()  # serve para pular linha
     f_contar_c('araujo')
